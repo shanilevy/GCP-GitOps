5b9c1ce2bc321ee77682683fd9d0702ff65f5bfa
Shani Levy <shaniiiiii@gmail.com>
2020-11-30 14:09:28 +1100

    Update main.tf
    test

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -7,12 +7,11 @@ resource "google_compute_firewall" "allow-http" {
   name    = "${local.network}-allow-http"
   network = local.network
   project = var.var_project
-
+  
   allow {
     protocol = "tcp"
     ports    = ["80"]
   }
-
   target_tags   = ["http-server"]
   source_ranges = ["0.0.0.0/0"]
 }
