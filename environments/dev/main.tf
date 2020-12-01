da57bcdb6e99e50ff7a476eb4c0e0c8a952f8a06
Shani Levy <shanilevy@google.com>
2020-12-01 11:13:26 +1100

    test

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -3,14 +3,16 @@ locals {
 }
 
 module "vpc" {
-  source  = "../../modules/vpc"
+  #source  = "../../modules/vpc"
+  source = "git::git@github.com:shanilevy/GCP-GitOps/blob/dev/modules/vpc"
   var_project = var.var_project
   var_region  = var.var_region
   var_env     = local.env
 }
 
 module "http_server" {
-  source  = "../../modules/http_server"
+  #source  = "../../modules/http_server"
+  source = "git::git@github.com:shanilevy/GCP-GitOps/blob/dev/modules/http_server"
   var_project = var.var_project
   var_machine_name = var.var_machine_name
   var_machine_size = var.var_machine_size
@@ -18,7 +20,8 @@ module "http_server" {
 }
 
 module "firewall" {
-  source  = "../../modules/firewall"
+  #source  = "../../modules/firewall"
+  source = "git::git@github.com:shanilevy/GCP-GitOps/blob/dev/modules/firewall"
   var_project = var.var_project
   var_subnet  = module.vpc.subnet
 }
